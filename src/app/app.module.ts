517733fab978a9a866b331a2840cb6a1442e5c10
Martin <martin@Martins-MacBook-Pro-2.local>
2020-01-24 11:04:23 +0100

    Starting to work on the front

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,33 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { RgpdComponent } from './home/steps/rgpd/rgpd.component';
+import { ContactFormComponent } from './home/steps/contact-form/contact-form.component';
+import { OperationDetailsComponent } from './home/steps/operation-details/operation-details.component';
+import { UploadFormComponent } from './home/steps/upload-form/upload-form.component';
+import { MatToolbarModule, MatCardModule, MatStepperModule, MatFormFieldModule } from '@angular/material';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    RgpdComponent,
+    ContactFormComponent,
+    OperationDetailsComponent,
+    UploadFormComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatCardModule,
+    MatStepperModule,
+    MatFormFieldModule
   ],
   providers: [],
   bootstrap: [AppComponent]
